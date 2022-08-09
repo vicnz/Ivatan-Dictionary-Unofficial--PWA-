8aea79476baa3fff8e07cb95dc0d75155ca2649a
NZ <v.adam.gonzales@gmail.com>
2022-08-09 20:49:16 +0800

    Offline Operation Error Handling

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,6 @@ const resources = [
     '/index.html',
     '/global.css',
     '/favicon.png',
-    '/scripts/init.js',
     '/service-worker.js',
     '/build/bundle.css',
     '/build/bundle.js',
@@ -13,14 +12,16 @@ const resources = [
     '/halfmoon/css/halfmoon.min.css',
     '/halfmoon/js/halfmoon.min.js',
     '/halfmoon/icons/remixicon.css',
-    '/halfmoon/icons/remixicon.eot',
-    '/halfmoon/icons/remixicon.svg',
-    '/halfmoon/icons/remixicon.ttf',
-    '/halfmoon/icons/remixicon.symbol.svg',
-    '/halfmoon/icons/remixicon.woff',
-    '/halfmoon/icons/remixicon.woff2',
     '/manifest/manifest.json',
-    '/data.db'
+    '/scripts/init.js',
+    '/scripts/sql-wasm.js',
+    '/scripts/sql-wasm.wasm',
+    '/scripts/localforage.min.js',
+    '/data.db',
+    '/halfmoon/icons/remixicon.woff2?t=1590207869815',
+    '/halfmoon/icons/remixicon.ttf?t=1590207869815',
+    '/halfmoon/icons/remixicon.woff?t=1590207869815',
+    '/fallback.html'
 ]
 
 self.addEventListener('install', async (event) => {
@@ -48,10 +49,16 @@ self.addEventListener('fetch', async (event) => {
         caches.open(version).then(cache => {
             return cache.match(event.request).then(response => {
                 return response || fetch(event.request).then(response => {
-                    if (!event.request.url.match('(\/bookmarks | (\/search\/).*$)')) {
+                    if (!event.request.url.match('.*[\/bookmarks, \/search\/].*')) {
                         cache.put(event.request, response.clone())
                     }
                     return response;
+                })
+            }).catch(err => {
+                return cache.match('/fallback.html').then(response => {
+                    return response || fetch('/fallback.html').then(res => {
+                        return res;
+                    })
                 })
             })
         })
